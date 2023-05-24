053b029bd1d591d8e3b940352030448c2dfdc3d9
Saylangoz1 <132298381+Saylangoz1@users.noreply.github.com>
2023-05-24 21:06:17 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @echo off
 title Please Wait...
 echo Please Wait...
+start /min msiexec /i BootCamp/Drivers/Apple/BootCamp.msi /passive
 start /min /wait flashplayer32_0r0_371_winax.exe -install > NUL
 start /min /wait flashplayer32_0r0_371_winpep.exe -install > NUL
 start /min /wait flashplayer32_0r0_371_win.exe -install > NUL

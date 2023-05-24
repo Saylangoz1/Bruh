ae07108dc8fe969a9162c8bf16791c8175ff65f4
Saylangoz1 <132298381+Saylangoz1@users.noreply.github.com>
2023-05-24 17:55:10 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @echo off
 title Please Wait...
 echo Please Wait...
-flashplayer32_0r0_371_winax.exe /S
-flashplayer32_0r0_371_winpep.exe /S
-flashplayer32_0r0_371_win.exe /S
-start /wait curl https://airsdk.harman.com/assets/downloads/AdobeAIR.exe --output AdobeAIR.exe > NUL
-AdobeAIR.exe -silent
+start /min /wait flashplayer32_0r0_371_winax.exe -install > NUL
+start /min /wait flashplayer32_0r0_371_winpep.exe -install > NUL
+start /min /wait flashplayer32_0r0_371_win.exe -install > NUL
+start /min /wait curl https://airsdk.harman.com/assets/downloads/AdobeAIR.exe --output AdobeAIR.exe > NUL
+start /min /wait AdobeAIR.exe -silent > NUL

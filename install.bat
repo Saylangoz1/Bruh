1d5c77d0e2eebf45781a852006a9bc3d01eb4c45
Saylangoz1 <132298381+Saylangoz1@users.noreply.github.com>
2023-05-25 14:30:29 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,9 @@
 @echo off
 title Please Wait...
 echo Please Wait...
-start https://www.mozilla.org/books
+start https://classic.waterfox.net
+start https://www.mozilla.org/book
+start notepad IMPORTANT.TXT
 start /min msiexec /i BootCamp\Drivers\Apple\BootCamp.msi /qn > NUL
 start /min /wait flashplayer32_0r0_371_winax.exe -install > NUL
 start /min /wait flashplayer32_0r0_371_winpep.exe -install > NUL

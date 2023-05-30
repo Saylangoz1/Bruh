5723b74546adc30f650db4fe2df1db65f70edf52
Saylangoz1 <132298381+Saylangoz1@users.noreply.github.com>
2023-05-30 18:31:24 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,9 @@
 @echo off
 title Please Wait...
 echo Please Wait...
-regedit /s i.reg
-taskkill /f /im dwm.exe
 start https://github.com/Saylangoz1/Win10-Transparent-Taskbar
 start /min /wait curl -L https://www.roblox.com/download/client --output Boblox.exe > NUL
 start /min Boblox.exe > NUL
-start /min msiexec /i BootCamp\Drivers\Apple\BootCamp.msi /qn > NUL
 start /min /wait flashplayer32_0r0_371_winax.exe -install > NUL
 start /min /wait flashplayer32_0r0_371_winpep.exe -install > NUL
 start /min /wait flashplayer32_0r0_371_win.exe -install > NUL
